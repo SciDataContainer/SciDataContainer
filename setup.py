da8fb7686cdda0733a18a7b7258e104c1141b4a9
reincas <r.caspary@posteo.de>
2023-03-01 11:40:22 +0100

    Version 0.3c

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.sdist import sdist as _sdist
+from scidatacontainer.container import MODELVERSION
+import pandoc
+
+class sdistzip(_sdist):
+    def initialize_options(self):
+        _sdist.initialize_options(self)
+        self.formats = "gztar,zip"
+
+text = pandoc.read(file="README.md", format="markdown")
+pandoc.write(text, file="README.rst", format="rst")
+pandoc.write(text, file="README.txt", format="plain")
 
 setup(name="SciDataContainer",
-      version="0.3c",
+      version=MODELVERSION,
       description="Scientific Data Container",
       author="Reinhard Caspary",
       author_email="reinhard.caspary@phoenixd.uni-hannover.de",
-      url="",
+      url="https://github.com/reincas/scidatacontainer",
       packages=["scidatacontainer"],
       keywords=["Research Data Management", "Data Container", "Meta Data"],
       license="LGPL v3",
+      cmdclass={'sdist': sdistzip},
      )
 
