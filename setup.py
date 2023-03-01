941c0dfa0ae6ca33583cd8b633a40a22fe3b09e3
reincas <r.caspary@posteo.de>
2023-03-01 17:17:03 +0100

    Version 0.3c

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
 
-import setuptools
+import setuptools  # required for command bdist_wheel
 from distutils.core import setup
-from scidatacontainer.container import MODELVERSION
+
+pkg = "scidatacontainer"
+with open(pkg + "/container.py", "r") as fp:
+    for line in fp.readlines():
+        if line[:15] == "MODELVERSION = ":
+            version = line[15:].split()[0].strip()
+            break
+    else:
+        raise RuntimeError("MODELVERSION missing in container.py!")
+
 with open("README.md", "r") as fp:
     readme = fp.read()
 
 setup(name="SciDataContainer",
-      version=MODELVERSION,
+      version=version,
       description="A container class for the storage of scientific data together with meta data",
       long_description=readme,
       long_description_content_type='text/markdown',
       author="Reinhard Caspary",
       author_email="reinhard.caspary@phoenixd.uni-hannover.de",
       url="https://github.com/reincas/scidatacontainer",
-      packages=["scidatacontainer"],
+      packages=[pkg],
       keywords=["Research Data Management", "Data Container", "Meta Data"],
       license="GNU Lesser General Public License (LGPL)",
       classifiers=[
